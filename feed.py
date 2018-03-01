6e16948cb364fda699fd55871da3c9c1314870c3
kfonseca <karstenfonseca@gmail.com>
2018-03-01 09:44:30 -0800

    Dumb mistake

--- feed.py
+++ feed.py
@@ -4,12 +4,8 @@ newsapi = NewsApiClient(api_key='21b90e53af6a4713baf4fd0d3d33cd2b')
 
 
 def getNews():
-    top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
-    print(top_headlines)
+    top = apnewsapii.get_top_headlines(sources='bbc-news')
+    print(top)
 
 
 
