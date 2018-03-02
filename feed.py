4f3c762c78e3685828dcc04841deed314f3bb92d
kfonseca <karstenfonseca@gmail.com>
2018-03-01 22:58:08 -0800

    added more sources

--- feed.py
+++ feed.py
@@ -4,18 +4,39 @@ newsapi = NewsApiClient(api_key='21b90e53af6a4713baf4fd0d3d33cd2b')
 
 
 def getNews():
-    top = newsapi.get_top_headlines(sources='bbc-news')
+    get_theVerge()
+    
+def get_arsTechnica():
+    top = newsapi.get_top_headlines(sources='ars-technica')
     readJson(top)
     # with open('data.txt', 'w') as outfile:  
     #     json.dump(top, outfile)
 
+def get_engadget():
+     top = newsapi.get_top_headlines(sources='engadget')
+     readJson(top)
 
+def get_reuters():
+     top = newsapi.get_top_headlines(sources='reuters')
+     readJson(top)
+
+def get_theVerge():
+     top = newsapi.get_top_headlines(sources='the-verge')
+     readJson(top)
+
+def get_wired():
+     top = newsapi.get_top_headlines(sources='wired')
+     readJson(top)
+
+def get_usTech():
+     top = newsapi.get_top_headlines(category='technology', country='us')
+     readJson(top)
 
 def readJson(inputJson):
     articles = inputJson["articles"]
     for i in articles:
-        articleTitle = i["title"]
-        print(articleTitle)
+        # articleTitle = i["title"]
+        print(i)
 
 
 if __name__ == '__main__':
