d0a33ddab0a5315f4ea12167d8171a5084c58c19
kfonseca <karstenfonseca@gmail.com>
2018-03-02 13:22:21 -0800

    firebase connected

--- feed.py
+++ feed.py
@@ -1,9 +1,35 @@
 from newsapi import NewsApiClient
+import pyrebase
 import json
+import datetime
+# Firebase admin packages
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# cred = credentials.Certificate("path/to/serviceAccountKey.json")
+# firebase_admin.initialize_app(cred)
+
+#  Class variables 
 newsapi = NewsApiClient(api_key='21b90e53af6a4713baf4fd0d3d33cd2b')
+# firebase = None
+
+# def initFirebase():
+#     config = {
+#         "apiKey": "AIzaSyBj3mbnomNrReTYcUG8rvz-KJ0FfTXA0w4",
+#         "authDomain": "feed-b8503.firebaseapp.com",
+#         "databaseURL": "https://feed-b8503.firebaseio.com",
+#         "projectId": "feed-b8503",
+#         "storageBucket": "feed-b8503.appspot.com",
+#         "messagingSenderId": "519738845227"
+#     }
+#     firebase = pyrebase.initialize_app(config)
+
+
 
 
 def getNews():
+    # initFirebase()
     get_theVerge()
     
 def get_arsTechnica():
@@ -34,10 +60,29 @@ def get_usTech():
 
 def readJson(inputJson):
     articles = inputJson["articles"]
-    for i in articles:
-        # articleTitle = i["title"]
-        print(i)
+    addToFirebase(articles)
+    # for i in articles:
+    #     # articleTitle = i["title"]
+    #     print(i)
 
+
+def addToFirebase(data):
+    config = {
+        "apiKey": "AIzaSyBj3mbnomNrReTYcUG8rvz-KJ0FfTXA0w4",
+        "authDomain": "feed-b8503.firebaseapp.com",
+        "databaseURL": "https://feed-b8503.firebaseio.com",
+        "projectId": "feed-b8503",
+        "storageBucket": "feed-b8503.appspot.com",
+        "messagingSenderId": "519738845227"
+    }
+    firebase = pyrebase.initialize_app(config)
+
+
+    # curDate = datetime.datetime.today().strftime('%Y-%m-%d')
+    dataString = "Articles"
+    db = firebase.database()
+    # ref = db.reference('server/saving-data/fireblog')
+    results = db.child(dataString).set(data)
 
 if __name__ == '__main__':
     getNews()
