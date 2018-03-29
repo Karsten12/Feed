ffbaee3354704ffd10f2c3c3bb5f12dfb069381f
kfonseca <karstenfonseca@gmail.com>
2018-03-28 20:39:03 -0700

    reformat python program

--- feed.py
+++ feed.py
@@ -17,36 +17,32 @@ from watson_developer_cloud.natural_language_understanding_v1 \
 #  Class variables 
 newsapi = NewsApiClient(api_key='21b90e53af6a4713baf4fd0d3d33cd2b')
 # IBM auth
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-    username='58efdfd2-95dc-4beb-a6ad-0849d87e8883',
-    password='rCt17FL22Usx',
-    version='2018-03-16'
-)
+natural_language_understanding = 0
 
-def initFirebase_viaADMIN():
+def initFirebase():
     # Fetch the service account key JSON file contents
-    cred = credentials.Certificate('./feedKEY.json')
+    cred = credentials.Certificate('./firebase-KEY.json')
     # Initialize the app with a service account, granting admin privileges
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://feed-b8503.firebaseio.com"
     })
     getNews()
 
-# def initFirebase_viaPYRE(){
-#     config = {
-#     "apiKey": "AIzaSyBj3mbnomNrReTYcUG8rvz-KJ0FfTXA0w4",
-#     "authDomain": "feed-b8503.firebaseapp.com",
-#     "databaseURL": "https://feed-b8503.firebaseio.com",
-#     "projectId": "feed-b8503",
-#     "storageBucket": "feed-b8503.appspot.com",
-#     "messagingSenderId": "519738845227"
-#     }
-#     # firebase = pyrebase.initialize_app(config)
-#     # db = firebase.database()
-#     results = db.reference("Articles").child(dataSource).set(data)
-# }
+def readIBMKEY():
+    global natural_language_understanding 
+    IBMKEYS = None 
+    fileName = "IBM-KEY.txt"
+    with open(fileName) as f:
+        content = f.readlines()
+        IBMKEYS = [x.strip() for x in content]
 
-def IBM(inputText):
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        username=IBMKEYS[0],
+        password=IBMKEYS[1],
+        version='2018-03-16'
+    )  
+
+def IBM():
     response = natural_language_understanding.analyze(
         # text=inputText
         text='IBM is an American multinational technology company '
@@ -81,6 +77,9 @@ def getNews():
 def addToFirebase(dataSource, data):
     results = db.reference("Articles").child(dataSource).set(data)
 
+      
+
 if __name__ == '__main__':
-    initFirebase_viaADMIN()
-    # IBM()
+    # initFirebase()
+    readIBMKEY()
+    IBM()
